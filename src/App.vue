49d0e0228e7aa6f07963f2ad51b0ea3e9e52e218
IntOverflow-maxwell <maxwell.miao@outlook.com>
2022-05-15 15:50:44 +0800

    Adding the latest version of the compiled scuffed_battleship for online testing of the chat and network functions.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <a href="./scuffed_battleship/index.html">Worst Chatroom Ever</a>
 </template>
 
 <script>
